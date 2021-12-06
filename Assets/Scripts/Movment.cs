27186b0d39272e71a3918aff8c06598231aa9655
Adrien <poignet@et.esiea.fr>
2021-12-06 17:43:02 +0100

    movment basique

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
+
+    [SerializeField] private Component m_MainCamera;
+    [SerializeField] private int m_VerticalSpeed;
+    [SerializeField] private int m_HorizontalSpeed;
+
+
+    void PlayerControl()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+            transform.position += new Vector3(0, 0.3f, 0);
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+            transform.position += new Vector3(0, -0.3f, 0);
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+            transform.position += new Vector3(0.3f, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+            transform.position += new Vector3(-0.3f, 0, 0);
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +29,8 @@ public class Movment : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        PlayerControl();
     }
 }
