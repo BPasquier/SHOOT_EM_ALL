9922ba88b4973c1bfa7d387a9d7232eb2e86b6cf
Adrien <poignet@et.esiea.fr>
2021-12-07 10:13:43 +0100

    marche pas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,10 +15,10 @@ public class Player : MonoBehaviour
     void PlayerControl()
     {
         ScreenPos = Camera.main.WorldToScreenPoint(transform.position);
-        if (Input.GetKey(KeyCode.UpArrow) == true &&  ScreenPos.y < Screen.height *2/3)
-            transform.position += new Vector3(0, 0.1f, 0);
-        if (Input.GetKey(KeyCode.DownArrow) == true && ScreenPos.y > 25)
-            transform.position += new Vector3(0, -0.2f, 0);
+        if (Input.GetKey(KeyCode.UpArrow) == true &&  ScreenPos.z < Screen.height *2/3)
+            transform.position += new Vector3(0, 0, 0.1f);
+        if (Input.GetKey(KeyCode.DownArrow) == true && ScreenPos.z > 4.8f)
+            transform.position += new Vector3(0, 0, -0.2f);
         if (Input.GetKey(KeyCode.RightArrow) == true && ScreenPos.x < Screen.width -40)
             transform.position += new Vector3(0.15f, 0, 0);
         if (Input.GetKey(KeyCode.LeftArrow) == true && ScreenPos.x > 40)
