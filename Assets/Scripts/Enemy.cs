e1e215fc7b231ac1720e093936593c7651d477d5
Leboullenger <luisleboullenger@gmail.com>
2021-12-08 11:46:56 +0100

    Terrain défile, attack du rattata
    - Animation pour le rattata qui attack et modification de ses behaviours dans le script ennemy.
    - Animation du terrain

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,22 +8,42 @@ public class Enemy : Entity
     float speed;
     [SerializeField]
     float timeAlive;
+    [SerializeField]
+    float range;
+    [SerializeField]
+    float dureeAttack;
+    Animator anim;
 
+    float timeLastCollision;
     float sinceBorn;
 
     private void Start()
     {
         sinceBorn = Time.time;
+        anim = GetComponent<Animator>();
+        timeLastCollision = Time.time - dureeAttack;
     }
     // Update is called once per frame
     void FixedUpdate()
-    {
-        transform.position += new Vector3(0f, 0f, -speed);
+    {        
+        if (timeLastCollision + dureeAttack < Time.time) //lorsque le rattata a finit d'attaquer, cours
+            transform.position += new Vector3(0f, 0f, -speed);
     }
     private void Update()
-    {
+    {        
+
         if (Time.time > sinceBorn + timeAlive)
             Destroy(gameObject);
         //or if hors champ
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("OnCollisionEnter()");
+        if (range <= 1f && collision.transform.tag == "Player")
+        {
+            Debug.Log("attack");
+            anim.SetBool("attack", true);
+            timeLastCollision = Time.time;
+        }
     }
 }
