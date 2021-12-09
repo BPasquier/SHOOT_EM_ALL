068ff78c657180492aaa019523e688bc893589ad
Baptiste <baptiste.pasquier8@gmail.com>
2021-12-09 11:14:36 +0100

    Ajout Attaque 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Diagnostics;
 
 public class Player : Entity
 {
@@ -9,11 +10,14 @@ public class Player : Entity
     [SerializeField] private Component m_MainCamera;
     [SerializeField] private Vector3 ScreenPos; 
     [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject Spell1;
+    [SerializeField] private long Spell1_ReloadTime;
     [SerializeField] private GameObject Score_Text;
     [SerializeField] private int m_VerticalSpeed;
     [SerializeField] private int m_HorizontalSpeed;
     [SerializeField] float timeBetweenBullet;
     public int score;
+    Stopwatch stopWatch = new Stopwatch();
     void PlayerControl()
     {
         ScreenPos = Camera.main.WorldToScreenPoint(transform.position);
@@ -25,6 +29,13 @@ public class Player : Entity
             transform.position += new Vector3(0.15f, 0, 0);
         if (Input.GetKey(KeyCode.LeftArrow) == true && ScreenPos.x > 40)
             transform.position += new Vector3(-0.15f, 0, 0);
+        print(stopWatch.ElapsedMilliseconds);
+        if (Input.GetKey(KeyCode.Keypad0) && stopWatch.ElapsedMilliseconds>Spell1_ReloadTime)
+        {
+            stopWatch.Reset();
+            stopWatch.Start();
+            GameObject spell = Instantiate(Spell1, transform.position + new Vector3(0f, 1f, 1f), Quaternion.Euler(90f,0f,0f));
+        }
     }
 
     // Update is called once per frame
@@ -38,6 +49,7 @@ public class Player : Entity
         HP = HP_Max;
         Enemy.OnBulletHit += Score;
         StartCoroutine(SpawnBullet());
+        stopWatch.Start();
     }
 
     IEnumerator SpawnBullet()
@@ -46,7 +58,6 @@ public class Player : Entity
         {
             GameObject obj = Instantiate(bullet, transform.position + new Vector3(0f, 0f, 1f), Quaternion.Euler(0f,0f,0f));
             yield return new WaitForSeconds(timeBetweenBullet);
-        
         }
     }
 
